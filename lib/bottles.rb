5be5c77c463e2a481355f692b8221f5e95e439c9
James Laverack <james@jameslaverack.com>
2017-08-13 15:49:23 +0100

    Add another method

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,15 +2,25 @@ class Bottles
   def verse(num)
     <<-VERSE
 #{plural(num)} of beer on the wall, #{plural(num)} of beer.
-Take one down and pass it around, #{plural(num - 1)} of beer on the wall.
-VERSE
+#{retrieve_bottles(num)}, #{plural(num - 1)} of beer on the wall.
+    VERSE
+  end
+
+  def retrieve_bottles(amount_remaining)
+    if amount_remaining > 1
+      'Take one down and pass it around'
+    elsif amount_remaining > 0
+      'Take it down and pass it around'
+    end
   end
 
   def plural(num)
     if num > 1
       "#{num} bottles"
-    else
+    elsif num == 1
       "#{num} bottle"
+    else
+      'no more bottles'
     end
   end
 end
