7e41a7592eb0d3ada37150ee206bc8f7ab95ebf8
James Laverack <james@jameslaverack.com>
2017-08-13 15:43:23 +0100

    Puluralise

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,16 @@
 class Bottles
   def verse(num)
     <<-VERSE
-#{num} bottles of beer on the wall, #{num} bottles of beer.
-Take one down and pass it around, #{num - 1} bottles of beer on the wall.
+#{plural(num)} of beer on the wall, #{plural(num)} of beer.
+Take one down and pass it around, #{plural(num - 1)} of beer on the wall.
 VERSE
+  end
+
+  def plural(num)
+    if num > 1
+      "#{num} bottles"
+    else
+      "#{num} bottle"
+    end
   end
 end
